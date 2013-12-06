4af7993f9c75a5f702702e9c19b8e431189cf9cb
c9s <cornelius.howl@gmail.com>
2013-12-06 21:15:57 +0800

    update UserBundle naming

--- Controller/LoginController.php
+++ Controller/LoginController.php
@@ -1,14 +1,12 @@
 <?php
 namespace AdminUI\Controller;
 use Phifty\Controller;
-use User\User;
 
 class LoginController extends Controller
 {
     public function indexAction()
     {
-        $bundle = User::getInstance();
-        // redirect
+        $bundle = \UserBundle\Main::getInstance();
         $goto = $this->request->param('f'); /* get both from POST and GET */
         return $this->render( '@AdminUI/login.html' , array( 
             'goto' => $goto , 
