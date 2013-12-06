408bfcf6012344b80c308ca2cab0a2462b7bbc5c
c9s <cornelius.howl@gmail.com>
2013-12-06 17:11:49 +0800

    Rename all $plugin to $bundle

--- Controller/LoginController.php
+++ Controller/LoginController.php
@@ -7,12 +7,12 @@ class LoginController extends Controller
 {
     public function indexAction()
     {
-        $plugin = User::getInstance();
+        $bundle = User::getInstance();
         // redirect
         $goto = $this->request->param('f'); /* get both from POST and GET */
         return $this->render( '@AdminUI/login.html' , array( 
             'goto' => $goto , 
-            'Plugin' => $plugin ) );
+            'Plugin' => $bundle ) );
     }
 }
 
