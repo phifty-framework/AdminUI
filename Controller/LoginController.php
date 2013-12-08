89d1b6ba58f82e0e823d48b63401932656c9e0cb
c9s <cornelius.howl@gmail.com>
2013-12-08 09:35:55 +0800

    revert Main to UserBundle class

--- Controller/LoginController.php
+++ Controller/LoginController.php
@@ -1,12 +1,13 @@
 <?php
 namespace AdminUI\Controller;
+use UserBundle\UserBundle;
 use Phifty\Controller;
 
 class LoginController extends Controller
 {
     public function indexAction()
     {
-        $bundle = \UserBundle\Main::getInstance();
+        $bundle = UserBundle::getInstance();
         $goto = $this->request->param('f'); /* get both from POST and GET */
         return $this->render( '@AdminUI/login.html' , array( 
             'goto' => $goto , 
