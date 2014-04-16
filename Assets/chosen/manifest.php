eeab487a5e4cfc75fdd513f52dfa844610b67572
c9s <yoanlin93@gmail.com>
2014-04-16 14:43:35 +0800

    Update chosen

---
+++ Assets/chosen/manifest.php
@@ -0,0 +1,26 @@
+<?php return array (
+  'collections' => 
+  array (
+    0 => 
+    array (
+      'files' => 
+      array (
+        0 => 'chosen',
+      ),
+    ),
+    1 => 
+    array (
+      'css' => 
+      array (
+        0 => 'chosen/chosen.min.css',
+      ),
+    ),
+    2 => 
+    array (
+      'js' => 
+      array (
+        0 => 'chosen/chosen.jquery.min.js',
+      ),
+    ),
+  ),
+);
