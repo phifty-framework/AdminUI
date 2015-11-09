8126a97d5752723247ee8c9cd6bbd5b8b30570a6
c9s <yoanlin93@gmail.com>
2015-11-09 16:18:16 +0800

    Update CRUDMenuItemFactory for interative SPA menu

--- CRUDMenuItemFactory.php
+++ CRUDMenuItemFactory.php
@@ -6,13 +6,17 @@ class CRUDMenuItemFactory
 {
     static public function create($crudId, $label) {
         $menuItem = new MenuItem($label);
-        
+
         $menuItem->setLinkAttributes(array(
             'href' => '/bs/' . $crudId,
             'data-crud-id' => $crudId,
+
             'data-region-path' => "/bs/$crudId/crud/index",
             // let the main panel region to load the index region from CRUDHandler.
             'region' => array('path' => "/bs/$crudId/crud/index"),
+
+            'data-region' => 'pageContent',
+            'data-path' => "/bs/$crudId/crud/index",
         ));
         return $menuItem;
     }
