582aaf3401f46adc5cfc6e5702c38378a9f9204d
c9s <yoanlin93@gmail.com>
2015-11-22 00:55:03 +0800

    php cs fix StackView class

--- Action/View/StackView.php
+++ Action/View/StackView.php
@@ -1,18 +1,29 @@
 <?php
+
 namespace AdminUI\Action\View;
+
 use FormKit;
 use FormKit\Layout\FieldsetLayout;
 use FormKit\Widget\HiddenInput;
+use FormKit\Element\Form;
 use FormKit\Element;
+use FormKit\Widget\SubmitInput;
 use DOMText;
-use ActionKit;
-
 
 /**
- * asView( '...', [ fields: [ ] ] )
- * asView( '...', [ skips: [ ] ] )
+ *  $view = new StackView($action, array(
+ *      'no_form' => true,
+ *      'no_signature' => true,
+ *      'form_id' => 'formId',
+ *      'form_class' => 'product-form',
+ *  ));
+ *  $view->buildRelationalActionViewForExistingRecords($relationId, $relation);
+ *  $view->buildRelationalActionViewForNewRecord($relationId,$relation);
+ *
+ * $action->asView( '...', [ fields: [ ] ] )
+ * $action->asView( '...', [ skips: [ ] ] ).
  */
-class StackView extends ActionKit\View\StackView
+class StackView extends \ActionKit\View\StackView
 {
     public $ajax = false;
     public $method = 'POST';
@@ -20,29 +31,30 @@ class StackView extends ActionKit\View\StackView
 
     public function createLayout()
     {
-        return new \FormKit\Layout\FieldsetLayout;
+        return new FieldsetLayout();
     }
 
     public function createContainer()
     {
-        $form = new FormKit\Element\Form;
+        $form = new Form();
         $form->enctype('multipart/form-data');
         $form->method($this->method);
 
         if ($this->option('ajax')) {
             $form->addClass('ajax-action');
         }
-        if( $this->ajax ) {
-            $ajaxFlag  = new FormKit\Widget\HiddenInput('__ajax_request',array( 'value' => '1' ));
-            $form->addChild( $ajaxFlag );
+        if ($this->ajax) {
+            $ajaxFlag = new HiddenInput('__ajax_request', array('value' => '1'));
+            $form->addChild($ajaxFlag);
         }
+
         return $form;
     }
 
     public function build()
     {
         // for each widget, push it into stack
-        $this->registerWidgets( $this->getAvailableWidgets() );
+        $this->registerWidgets($this->getAvailableWidgets());
         if ($this->option('nested') && $this->action->nested) {
             $this->buildNestedSection();
         }
@@ -52,11 +64,11 @@ class StackView extends ActionKit\View\StackView
     {
         $widgets = parent::getAvailableWidgets();
         $record = $this->getRecord();
-        if ( $record && $record->id ) {
+        if ($record && $record->id) {
             return $widgets;
         }
         // XXX: hack
-        return array_filter($widgets, function($w) {
+        return array_filter($widgets, function ($w) {
             return $w->name != 'id';
         });
     }
@@ -65,31 +77,31 @@ class StackView extends ActionKit\View\StackView
     {
         parent::afterBuild();
         $container = $this->getContainer();
-        $signature = new HiddenInput('__action',array(
-            'value' => $this->action->getSignature()
+        $signature = new HiddenInput('__action', array(
+            'value' => $this->action->getSignature(),
         ));
-        $container->addChild( $signature );
-        $container->addChild( $this->buildButtonGroup() );
+        $container->addChild($signature);
+        $container->addChild($this->buildButtonGroup());
     }
 
     public function buildButtonGroup()
     {
         $record = $this->getRecord();
         $recordId = $record ? $record->id : null;
-        $isCreate = ! $recordId;
+        $isCreate = !$recordId;
 
         $buttonGroup = new Element('div');
-        $buttonGroup->setAttributes(array('class' => 'button-group') );
+        $buttonGroup->setAttributes(array('class' => 'button-group'));
 
         if ($this->option('submit_btn')) {
             if ($isCreate) {
-                $submit = new \FormKit\Widget\SubmitInput(null, array(
+                $submit = new SubmitInput(null, array(
                     'class' => 'formkit-widget formkit-widget-submit create button',
                     'value' => $this->option('submit_label') ?: _('Create'),
                 ));
                 $buttonGroup->addChild($submit);
             } else {
-                $submit = new \FormKit\Widget\SubmitInput( null, array( 
+                $submit = new SubmitInput(null, array(
                     'class' => 'formkit-widget formkit-widget-submit create button',
                     'value' => $this->option('submit_label') ?: _('Save'),
                 ));
@@ -99,18 +111,17 @@ class StackView extends ActionKit\View\StackView
 
         // button for closing edit region
         if ($this->option('close_btn')) {
-            $fadeRemoveButton = new FormKit\Widget\ButtonInput( null, array( 
+            $fadeRemoveButton = new ButtonInput(null, array(
                 'class' => 'formkit-widget formkit-widget-button button record-close-btn',
                 'value' => $this->option('cancel_label') ?: _('Cancel'),
                 'onclick' => '(function(o) {
                     Phifty.CRUD.closeEditRegion(o);
                 })(this);',
             ));
-            $buttonGroup->addChild( new DOMText("\n") );
-            $buttonGroup->addChild( $fadeRemoveButton );
+            $buttonGroup->addChild(new DOMText("\n"));
+            $buttonGroup->addChild($fadeRemoveButton);
         }
+
         return $buttonGroup;
     }
-
 }
-
