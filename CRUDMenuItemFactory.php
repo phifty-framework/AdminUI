5022f023daf9e70e923b7cd45d5db33592f61724
c9s <yoanlin93@gmail.com>
2015-11-23 01:01:05 +0800

    Add more CRUD link helpers

--- CRUDMenuItemFactory.php
+++ CRUDMenuItemFactory.php
@@ -4,9 +4,48 @@ use WebUI\Components\Menu\MenuItem;
 
 class CRUDMenuItemFactory
 {
+
+
+    static public function viewRecordPage($crudId, $recordId, $label)
+    {
+        $queryString = http_build_query([ 'id' => intval($recordId), '_form_controls' => true, ]);
+        $menuItem = new MenuItem($label);
+        $menuItem->setLinkAttributes(array(
+            'href' => '/bs/' . $crudId . '/view?' . $queryString,
+            'data-crud-id' => $crudId,
+            'data-region-path' => "/bs/$crudId/crud/view?" . $queryString,
+
+            // let the main panel region to load the index region from CRUDHandler.
+            'region' => array('path' => "/bs/$crudId/crud/view?" . $queryString),
+
+            'data-region' => 'pageContent',
+            'data-path' => "/bs/$crudId/crud/view?" . $queryString,
+        ));
+        return $menuItem;
+    }
+
+
+    static public function editRecordPage($crudId, $recordId, $label)
+    {
+        $queryString = http_build_query([ 'id' => intval($recordId), '_form_controls' => true, ]);
+        $menuItem = new MenuItem($label);
+        $menuItem->setLinkAttributes(array(
+            'href' => '/bs/' . $crudId . '/edit?' . $queryString,
+            'data-crud-id' => $crudId,
+            'data-region-path' => "/bs/$crudId/crud/edit?" . $queryString,
+
+            // let the main panel region to load the index region from CRUDHandler.
+            'region' => array('path' => "/bs/$crudId/crud/edit?" . $queryString),
+
+            'data-region' => 'pageContent',
+            'data-path' => "/bs/$crudId/crud/edit?" . $queryString,
+        ));
+        return $menuItem;
+    }
+
+
     static public function create($crudId, $label) {
         $menuItem = new MenuItem($label);
-
         $menuItem->setLinkAttributes(array(
             'href' => '/bs/' . $crudId,
             'data-crud-id' => $crudId,
